a6e2027a2b145a0cafabb16065b175170f049a54
ray <rayabra2011@gmail.com>
2026-02-23 11:53:18 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { ProductList } from './Component/ProductList';
+import { ProductCard } from './Component/ProductCard';
+import './App.css';
+
+function App() {
+  const products = [
+    {
+      imageSrc: 'images/iphone.png',
+      title: 'iPhone 15 Pro',
+      specification: [
+        'A17 Pro chip with 6-core GPU',
+        '3x or 5x Telephoto camera',
+        'Up to 29 hours video playback',
+      ],
+      price: 999,
+    },
+    {
+      imageSrc: 'images/airpods.png',
+      title: 'AirPods Pro 2',
+      specification: [
+        'Noise Cancellation',
+        'Dust, sweat, and water resistant',
+        'Up to 6 hours of listening',
+      ],
+      price: 249,
+    },
+    {
+      imageSrc: 'images/apple-watch.png',
+      title: 'Apple Watch 9',
+      specification: [
+        '45mm or 41mm case size',
+        'Always-On Retina display',
+        'Up to 18 hours normal use',
+      ],
+      price: 399,
+    },
+  ];
+
+  function handlePurchase(products) {
+    alert(`You Clicked on ${products.title} which cost $${products.price}`);
+  }
+
+  return (
+    <div className="App">
+      <ProductList>
+        {products.map((product) => (
+          <ProductCard
+            key={product.title}
+            product={product}
+            onPurchase={handlePurchase}
+          />
+        ))}
+      </ProductList>
+      <div>
+        <h2>Product which cost up to $500</h2>
+        <ul>
+          {products
+            .filter(({ price }) => price > 500)
+            .map(({ title, price }) => (
+              <li key={title}>
+                {title} - ${price}
+              </li>
+            ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
