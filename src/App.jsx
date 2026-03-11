6ab455e784357e8dcaec6246629dc383b662127e
ray <rayabra2011@gmail.com>
2026-03-11 22:37:39 +0700

    update project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { ProductList } from './Component/ProductList';
 import { ProductCard } from './Component/ProductCard';
-import './App.css';
+import { ProductFilter } from './Component/ProductFilter';
+import styles from './App.module.css';
 import { Fragment } from 'react/jsx-runtime';
+import { useState } from 'react';
 
 function App() {
   const products = [
     {
+      id: 1,
       imageSrc: 'images/iphone.png',
       title: 'iPhone 15 Pro',
       specification: [
@@ -13,9 +16,11 @@ function App() {
         '3x or 5x Telephoto camera',
         'Up to 29 hours video playback',
       ],
+      stockCount: 10,
       price: 999,
     },
     {
+      id: 2,
       imageSrc: 'images/airpods.png',
       title: 'AirPods Pro 2',
       specification: [
@@ -23,9 +28,11 @@ function App() {
         'Dust, sweat, and water resistant',
         'Up to 6 hours of listening',
       ],
+      stockCount: 0,
       price: 249,
     },
     {
+      id: 3,
       imageSrc: 'images/apple-watch.png',
       title: 'Apple Watch 9',
       specification: [
@@ -33,39 +40,75 @@ function App() {
         'Always-On Retina display',
         'Up to 18 hours normal use',
       ],
+      stockCount: 4,
       price: 399,
     },
   ];
 
+  const [favourites, setFavourites] = useState([]);
+
+  const [filters, setFilters] = useState({
+    price: {
+      min: 0,
+      max: 999,
+    },
+    other: 'other value',
+  });
+
+  function handleFavourite(productsId) {
+    if (favourites.includes(productsId)) {
+      setFavourites((prevFavourites) =>
+        prevFavourites.filter((id) => id !== productsId),
+      );
+    } else {
+      setFavourites((prevFavourites) => [...prevFavourites, productsId]);
+    }
+  }
+
+  function handleFilter(key, value) {
+    setFilters((prevFilters) => ({
+      price: {
+        ...prevFilters.price,
+        [key]: value,
+      },
+    }));
+  }
+
   function handlePurchase(products) {
-    alert(`You Clicked on ${products.title} which cost $${products.price}`);
+    alert(
+      `You Clicked on ${products.title} which cost $${products.price - 200}`,
+    );
   }
 
   return (
-    <div className="App">
+    <div className={styles.App}>
       <ProductList>
         {products.map((product) => (
           <ProductCard
             key={product.title}
             product={product}
+            isFavourite={favourites.includes(product.id)}
             onPurchase={handlePurchase}
+            onFavourite={handleFavourite}
           />
         ))}
       </ProductList>
 
-      <h2>Product which cost up to $500</h2>
-      <ul>
-        {products
-          .filter(({ price }) => price > 500)
-          .map(({ title, price }) => (
-            <Fragment key={title}>
-              <hr style={{ borderColor: 'slategray' }} />
-              <p>
-                {title} - ${price}
-              </p>
-            </Fragment>
-          ))}
-      </ul>
+      <h2>Product Filtered by Price </h2>
+      <ProductFilter filters={filters} onFilter={handleFilter} />
+      {products
+        .filter(
+          ({ price }) =>
+            price >= filters.price.min && price <= filters.price.max,
+        )
+        .map(({ title, price }) => (
+          <Fragment key={title}>
+            <hr className={styles.AppListDivider} />
+            <p className={styles.ListTitle}>
+              {title} - ${price}
+            </p>
+          </Fragment>
+        ))}
     </div>
   );
 }
--- src/Component/ProductCard.js
+++ src/Component/ProductCard.js
@@ -1,15 +1,34 @@
-export function ProductCard({ product, background = 'slategray', onPurchase }) {
+import { useState } from 'react';
+import styles from './ProductCard.module.css';
+
+export function ProductCard({
+  product,
+  isFavourite,
+  background = 'slategray',
+  onPurchase,
+  onFavourite,
+}) {
+  const [stockCount, setStockCount] = useState(product.stockCount);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleClick() {
+    setStockCount((prevstockCount) => prevstockCount - 1);
+    onPurchase(product);
+  }
+
+  function handleTwoClick() {
+    setStockCount((prevstockCount) => prevstockCount - 1);
+    setStockCount((prevstockCount) => prevstockCount - 1);
+  }
+
   return (
-    <article
-      style={{
-        background,
-        width: '100%',
-        border: '1px solid white',
-        borderRadius: '8px',
-        padding: '16px',
-        textAlign: 'center',
-      }}
-    >
+    <article className={styles.Container} style={{ background }}>
+      <button
+        className={styles.Favourite}
+        onClick={() => onFavourite(product.id)}
+      >
+        {isFavourite ? '❤️' : '🤍'}
+      </button>
       <h2> {product.title} </h2>
       <img
         src={product.imageSrc}
@@ -17,15 +36,42 @@ export function ProductCard({ product, background = 'slategray', onPurchase }) {
         width={128}
         height={128}
       />
-      <p>Specification:</p>
-      <ul style={{ listStyle: 'none', padding: 0 }}>
-        {product.specification.map((spec, index) => (
-          <li key={index}> {spec} </li>
-        ))}
-      </ul>
-      <button onClick={() => onPurchase(product)}>
-        Buy (From ${product.price})
-      </button>
+      <p>
+        Specification:{' '}
+        <button onClick={() => setShowMore(!showMore)}>
+          {showMore ? 'Hide' : 'Show'}
+        </button>{' '}
+      </p>
+      {showMore && (
+        <ul className={styles.Specification}>
+          {product.specification.map((spec, index) => (
+            <li key={index}> {spec} </li>
+          ))}
+        </ul>
+      )}
+      <Status stockCount={stockCount} />
+      {stockCount > 0 && (
+        <>
+          <p>
+            {' '}
+            (From ${product.price} to ${product.price - 200})
+          </p>
+          <button onClick={handleClick}>Buy</button>
+        </>
+      )}
+      {stockCount > 1 && <button onClick={handleTwoClick}>Buy 2</button>}
     </article>
   );
+}
+
+function Status({ stockCount }) {
+  const notAvailableTemplate = (
+    <p className={styles.NotAvailableStatus}>Not available</p>
+  );
+
+  const availableTemplate = (
+    <p className={styles.AvailableStatus}>{stockCount} Items available</p>
+  );
+
+  return stockCount === 0 ? notAvailableTemplate : availableTemplate;
 }
